cecb110f86574cfc934e22b0661e23cef8380ec0
AMirshekari <amirshekari@ebates.com>
2019-10-19 20:22:19 -0700

    Added List of All Streams

--- stream-app/client/src/components/streams/StreamList.js
+++ stream-app/client/src/components/streams/StreamList.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchStreams} from "../../actions";
 
-const StreamList = () => {
-    return (
-        <div>
-            StreamList
-        </div>
-    );
+class StreamList extends Component{
+
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderStreamList() {
+
+        return this.props.streams.map(stream => (
+            <div className="item" key={stream.id}>
+                <i className="large middle aligned icon camera" />
+                <div className="content">
+                    {stream.title}
+                    <div className="description">
+                        {stream.description}
+                    </div>
+                </div>
+            </div>
+        ));
+    }
+
+    render() {
+        
+        if(!this.props.streams.length) {
+            return <div>Loading List of All Streams...</div>;
+        }
+
+        return(
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderStreamList()}
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+
+    return {streams: Object.values(state.streams)};
 };
 
-export default StreamList;
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
