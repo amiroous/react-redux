5523e22b094ae02ee014ec438b3db444ecedf0de
AMirshekari <amirshekari@ebates.com>
2019-11-03 12:20:26 -0800

    Completed RTMP Video Streaming

--- stream-app/client/src/components/streams/StreamShow.js
+++ stream-app/client/src/components/streams/StreamShow.js
@@ -1,11 +1,71 @@
-import React, {Component} from 'react';
+import React, {Component, createRef} from 'react';
 import {connect} from "react-redux";
 import {fetchStream} from "../../actions";
+import flvjs from "flv.js";
 
 class StreamShow extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.videoRef = createRef();
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const {id} = this.props.match.params;
+
+        this.props.fetchStream(id);
+        this.createVideoPlayer();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.createVideoPlayer();
+    }
+
+    componentWillUnmount() {
+        this.videoPlayer.destroy();
+    }
+
+    async createVideoPlayer() {
+
+        if(this.videoPlayer || !this.props.stream) {
+            return;
+        }
+
+        const {id} = this.props.match.params;
+
+        this.videoPlayer = flvjs.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+
+        // this.videoPlayer.attachMediaElement(this.videoRef.current);
+        // this.videoPlayer.load();
+        // this.videoPlayer.play()
+        //     .then(() => {})
+        //     .catch((error) => {
+        //         if (error && error.toString().toLowerCase().includes('the play() request was interrupted by a call to pause()')) {
+        //             // Do Nothing
+        //         } else {
+        //             this.videoPlayer.destroy();
+        //             throw error;
+        //         }
+        //     });
+
+
+        this.videoPlayer.attachMediaElement(this.videoRef.current);
+        this.videoPlayer.load();
+
+        try {
+            await this.videoPlayer.play();
+        } catch (error) {
+            if (error && error.toString().toLowerCase().includes('the play() request was interrupted by a call to pause()')) {
+                // Do Nothing
+            } else {
+                this.videoPlayer.destroy();
+                // throw error;
+            }
+        }
     }
 
     render() {
@@ -18,6 +78,13 @@ class StreamShow extends Component {
 
         return (
             <div>
+                <video
+                    ref={this.videoRef}
+                    style={{
+                        width: '100%'
+                    }}
+                    controls
+                />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
